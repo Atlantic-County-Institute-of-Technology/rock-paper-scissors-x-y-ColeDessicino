9655007979dbdf9ab72ba0d8848f0c7e0754e163
Cole <dess0037@student.acitech.org>
2025-03-19 09:10:19 -0400

    I made the buttons work

---
+++ script.js
@@ -0,0 +1,54 @@
+const option_1 = document.getElementById("option_1");
+const option_2 = document.getElementById("option_2");
+const option_3 = document.getElementById("option_3");
+let image_you = document.getElementById("image_you");
+const rockid = document.getElementById("rock");
+const paperid = document.getElementById("paper");
+const scissorsid = document.getElementById("scissors");
+
+
+let clicksrock = 0;
+
+option_1.addEventListener("click", chooserock);
+
+// block disables none and none makes the image disappear
+function chooserock() {
+    clicksrock++;
+    if( clicksrock == 1 ) {
+        console.log("Rock");
+        rockid.style.display = "block";
+        paperid.style.display = "none";
+        scissorsid.style.display = "none";
+        clicksrock = 0
+    }
+}
+
+let clickspaper = 0;
+
+option_2.addEventListener("click", choosepaper);
+
+function choosepaper() {
+    clickspaper++;
+    if( clickspaper == 1 ) {
+        console.log("Paper");
+        rockid.style.display = "none";
+        paperid.style.display = "block";
+        scissorsid.style.display = "none";
+        clickspaper = 0
+    }
+}
+
+let clicksscissors = 0;
+
+option_3.addEventListener("click", choosescissors);
+
+function choosescissors() {
+    clicksscissors++;
+    if( clicksscissors == 1 ) {
+        console.log("scissors");
+        rockid.style.display = "none";
+        paperid.style.display = "none";
+        scissorsid.style.display = "block";
+        clicksscissors = 0
+    }
+}
